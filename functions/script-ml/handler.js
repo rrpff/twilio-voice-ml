a1f20f982361609ca2da129ae745f69127bf2a1d
Richard Foster <richard@madetech.co.uk>
2016-04-12 12:33:44 +0100

    Create twilio ML script

--- functions/script-ml/handler.js
+++ functions/script-ml/handler.js
@@ -1,7 +1,29 @@
 'use strict';
 
+var twilio = require('twilio');
+
 module.exports.handler = function(event, context) {
-  return context.done(null, {
-    message: 'Go Serverless! Your Lambda function executed successfully!'
+  console.log({
+    event: event,
+    context: context
+  });
+
+  var twiml = twilio.TwimlResponse()
+    .say('Hello, thanks for contacting Surfdome.')
+    .say('For information on your order, please type in your order number, followed by the pound key.')
+    .gather({
+      action: 'https://taf522husd.execute-api.us-east-1.amazonaws.com/production/twiml-find-order',
+      timeout: 10,
+      method: 'POST',
+      numDigits: 9
+    })
+    .say('Sorry, your order does not exist');
+
+  console.log({ twiml: twiml.toString() });
+
+  return context.succeed({
+    body: twiml.toString()
   });
 };
+
+module.exports.handler({}, { succeed: () => {} });
